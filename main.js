9091b875716518268d9e52da71341ffa180b456f
Arthur-IFPR <arthurvalentinscoparoifpr@gmail.com>
2026-09-13 18:18:40 -0300

    T2.P2.A11

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 import Voo from './Voo.js';
 import Aeroporto from './Aeroporto.js';
+import TorreDeControle  from './torre.js';
 
 let aeroportoCWB = new Aeroporto("Afonso Pena");
 
 let voo1 = new Voo("G3-100", "São Paulo");
+
 let voo2 = new Voo("LA-200", "Rio de Janeiro");
 
-// Testando o Desafio 1
 aeroportoCWB.adicionarVooNoRadar(voo1);
 aeroportoCWB.adicionarVooNoRadar(voo2);
 
-// Testando o Desafio 2
 let vooAchado = aeroportoCWB.buscarVoo("LA-200");
-console.log(vooAchado); // Tem que mostrar os dados do voo do Rio de Janeiro!
+console.log(vooAchado);
+
+let torre1 = new TorreDeControle();
+let torre2 = new TorreDeControle();
+
+console.log(torre1 === torre2); // true
